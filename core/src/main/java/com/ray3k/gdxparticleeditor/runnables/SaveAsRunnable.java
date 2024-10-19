b6aa409642aab52668f41c355d83f7ef3168f5f0
herve-ch <rv_changue@hotmail.com>
2024-10-18 20:25:00 -1000

    fix apple M1 crash on save (NSWindow geometry should only be modified on the main thread!)

--- core/src/main/java/com/ray3k/gdxparticleeditor/runnables/SaveAsRunnable.java
+++ core/src/main/java/com/ray3k/gdxparticleeditor/runnables/SaveAsRunnable.java
@@ -3,9 +3,6 @@ package com.ray3k.gdxparticleeditor.runnables;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.ray3k.gdxparticleeditor.FileDialogs;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 import static com.ray3k.gdxparticleeditor.Core.*;
 import static com.ray3k.gdxparticleeditor.Settings.*;
 
@@ -14,31 +11,31 @@ public class SaveAsRunnable implements Runnable {
     private static boolean open;
     private SaveRunnable saveRunnable;
 
-    public void setSaveRunnable (SaveRunnable runnable) {
+    public void setSaveRunnable(SaveRunnable runnable) {
         saveRunnable = runnable;
     }
 
     @Override
-    public void run () {
+    public void run() {
         if (open) return;
 
-        ExecutorService service = Executors.newSingleThreadExecutor();
-        service.execute(() -> {
-            open = true;
-            stage.getRoot().setTouchable(Touchable.disabled);
+        //ExecutorService service = Executors.newSingleThreadExecutor();
+        //service.execute(() -> {
+        open = true;
+        stage.getRoot().setTouchable(Touchable.disabled);
 
-            var useFileExtension = preferences.getBoolean(NAME_PRESUME_FILE_EXTENSION, DEFAULT_PRESUME_FILE_EXTENSION);
-            var filterPatterns = useFileExtension ? new String[] {"p"} : null;
-            var saveHandle = FileDialogs.saveDialog("Save", getDefaultSavePath(), defaultFileName, filterPatterns, "Particle Files (*.p)");
+        var useFileExtension = preferences.getBoolean(NAME_PRESUME_FILE_EXTENSION, DEFAULT_PRESUME_FILE_EXTENSION);
+        var filterPatterns = useFileExtension ? new String[]{"p"} : null;
+        var saveHandle = FileDialogs.saveDialog("Save", getDefaultSavePath(), defaultFileName, filterPatterns, "Particle Files (*.p)");
 
-            if (saveHandle != null) {
-                openFileFileHandle = saveHandle;
-                saveRunnable.run();
-            }
+        if (saveHandle != null) {
+            openFileFileHandle = saveHandle;
+            saveRunnable.run();
+        }
 
-            stage.getRoot().setTouchable(Touchable.enabled);
-            open = false;
-        });
-        service.shutdown();
+        stage.getRoot().setTouchable(Touchable.enabled);
+        open = false;
+        //});
+        //service.shutdown();
     }
 }
